1df4f48f779415541cbfd7b2550daf725bf41403
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2019-12-31 10:39:21 +0100

    [Fix] Broken settings

--- resources/lib/migration_utils.py
+++ resources/lib/migration_utils.py
@@ -27,9 +27,12 @@ from __future__ import unicode_literals
 
 import os
 import json
+import importlib
 from builtins import str
 from builtins import range
 from kodi_six import xbmcvfs
+import xml.etree.ElementTree as ET
+
 
 from codequick import storage, Script, listing
 
@@ -65,3 +68,109 @@ def migrate_from_pickled_fav():
         with open(fav_json_fp, 'w') as f:
             json.dump(new_fav_dict, f, indent=4)
         xbmcvfs.delete(fav_pickle_fp)
+
+
+def update_json(key, order, s, j, setting, item_infos):
+    set_setting = True
+    if setting == "":
+        set_setting = False
+    else:
+        if order:
+            if item_infos['order'] == int(setting):
+                set_setting = False
+        else:
+            if bool(setting):
+                set_setting = False
+
+    if set_setting:
+        if key not in j:
+            j[key] = {}
+        if s not in j[key]:
+            j[key][s] = {}
+        if order:
+            j[key][s]['order'] = int(setting)
+        else:
+            j[key][s]['hidden'] = not bool(setting)
+
+
+def migrate_old_menus_settings(menus_settings_fp):
+    """
+    This function moves old user settings concerning hidden menu
+    and items order in
+    the new json file used for that purpose.
+    The feature (json) appeared in 0.2.17~betaXX
+    All user with version >= 0.2.17 will use item settings in the JSON format
+    Maybe we can remove the migration check on version 0.2.20?
+    """
+
+    # If the json file already exists, the migration has already be done
+    if xbmcvfs.exists(menus_settings_fp):
+        return
+
+    # We directly read the XML setting file (faster than 'xbmcaddon.Addon().getSetting(s)')
+    addon_settings_fp = os.path.join(Script.get_info('profile'), "settings.xml")
+    if not xbmcvfs.exists(addon_settings_fp):
+        return
+
+    Script.log('Start item settings migration to json file')
+    j = {}
+
+    skeletons = [
+        "root",
+        "live_tv",
+        "replay",
+        'websites',
+        "fr_live",
+        "ch_live",
+        "uk_live",
+        "wo_live",
+        "be_live",
+        "jp_live",
+        "ca_live",
+        "us_live",
+        "pl_live",
+        "es_live",
+        "tn_live",
+        "it_live",
+        "nl_live",
+        "cn_live",
+        "fr_replay",
+        "ch_replay",
+        "uk_replay",
+        "wo_replay",
+        "be_replay",
+        "jp_replay",
+        "ca_replay",
+        "us_replay",
+        "es_replay",
+        "tn_replay",
+        "it_replay",
+        "nl_replay",
+        "cn_replay"
+    ]
+
+    tree = ET.parse(addon_settings_fp)
+    for elem in tree.findall('setting'):
+        if 'id' in elem.attrib:
+            setting_id = elem.attrib['id']
+            if 'value' in elem.attrib:
+                # Kodi 17 case
+                setting_value = elem.attrib['value']
+            else:
+                # Kodi 18 and 19 case
+                setting_value = elem.text
+            # print('Setting: "{}" --> "{}"'.format(setting_id, setting_value))
+            order = False
+            if '.order' in setting_id:
+                order = True
+                setting_id = setting_id.split('.')[0]
+            for skeleton in skeletons:
+                current_menu = importlib.import_module('resources.lib.skeletons.' +
+                                                       skeleton).menu
+                if setting_id in current_menu:
+                    item_infos = current_menu[setting_id]
+                    update_json(skeleton, order, setting_id, j, setting_value, item_infos)
+
+    # Save new item settings json
+    with open(menus_settings_fp, 'w') as f:
+        json.dump(j, f, indent=4)
